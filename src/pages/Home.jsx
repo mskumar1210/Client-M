c3640e57138ffe359ab244eee54c0d8c33006b01
Mayureshwar <mayureshwarkumar15@gmail.com>
2025-09-18 12:16:58 +0530

    frontend change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,9 +4,10 @@ import Courses from './Courses'
 function Home() {
   return (
     <>
-    <img src='https://pninfosys.org/bannerFinal.jpg' alt='' className='w-100' style={{height:'400px'}}/>
+    <img src='<a href="https://www.vecteezy.com/vector-art/10925712-waiting-room-with-ticket-system-customer-management-method-electronic-queuing-system-electronic-queue-management-vector-illustration">waiting-room-with-ticket-system-customer-management-method-electronic-queuing-system-electronic-queue-management-vector-illustration Vectors by Vecteezy</a>'
+         alt='' className='w-100' style={{height: '400px'}}/>
 
-    <Courses/>
+      <Courses/>
     </>
   )
 }
